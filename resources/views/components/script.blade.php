ebf3cff06afdf0cd65cb2b32b442f70e5b1b367d
saak01 <joaovictorcosta.dev@gmail.com>
2024-05-14 09:26:56 -0300

    setup components head and script

---
+++ resources/views/components/script.blade.php
@@ -0,0 +1,2 @@
+<script src="{{asset('node_modules/jquery/dist/jquery.js')}}"></script>
+<script src="{{asset('node_modules/@popperjs/core/dist/umd/popper.js')}}"></script>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,4 +4,5 @@
 <body>
 
 </body>
+@extends('components.script')
 </html>
